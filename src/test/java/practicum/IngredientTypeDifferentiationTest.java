91fab330912425b6b832dedced9ee2c073271669
Фарида Сулейманова <bulatovfar@mail.ru>
2025-09-12 16:21:37 +0300

    UnitTests

---
+++ src/test/java/practicum/IngredientTypeDifferentiationTest.java
@@ -0,0 +1,19 @@
+package practicum;
+
+import org.junit.Test;
+import ru.practicum.Ingredient;
+import ru.practicum.IngredientType;
+
+import static org.junit.Assert.assertNotEquals;
+
+public class IngredientTypeDifferentiationTest {
+
+    @Test
+    public void testIngredientTypeDifferentiation() {
+        Ingredient fillingIngredient = new Ingredient(IngredientType.FILLING, "cheese", 80);
+        Ingredient sauceIngredient = new Ingredient(IngredientType.SAUCE, "hot sauce", 50);
+
+        // Проверяем, что типы ингредиентов различны
+        assertNotEquals(fillingIngredient.getType(), sauceIngredient.getType());
+    }
+}
---
+++ src/test/java/practicum/IngredientTypeEnumTest.java
@@ -0,0 +1,24 @@
+package practicum;
+import org.junit.Test;
+import ru.practicum.IngredientType;
+
+import static org.junit.Assert.*;
+
+import java.util.EnumSet;
+
+
+public class IngredientTypeEnumTest {
+    // Тест проверки всех значений перечисления
+    @Test
+    public void testEnumValues() {
+        // Получаем все значения перечисления
+        IngredientType[] types = IngredientType.values();
+
+        // Проверяем, что в перечислении два значения
+        assertEquals(2, types.length); // Должно быть два значения
+
+        // Проверяем, что перечисление содержит нужные элементы
+        assertTrue(EnumSet.allOf(IngredientType.class).contains(IngredientType.SAUCE));
+        assertTrue(EnumSet.allOf(IngredientType.class).contains(IngredientType.FILLING));
+    }
+}
